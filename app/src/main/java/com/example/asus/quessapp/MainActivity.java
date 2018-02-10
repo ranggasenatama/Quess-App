41ec303d264fc85d4be48da0f102c83fa7bb1b80
Rangga <ranggasenatama@gmail.com>
2018-02-10 13:27:29 +0700

    layout

--- app/src/main/java/com/example/asus/quessapp/MainActivity.java
+++ app/src/main/java/com/example/asus/quessapp/MainActivity.java
@@ -1,9 +1,24 @@
 package com.example.asus.quessapp;
 
+import android.graphics.Bitmap;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
+
 public class MainActivity extends AppCompatActivity {
+
+    public class DownloadTask extends AsyncTask<String, Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... strings) {
+            String result = "";
+            URL url;
+            HttpURLConnection httpURLConnection;
+        }
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
