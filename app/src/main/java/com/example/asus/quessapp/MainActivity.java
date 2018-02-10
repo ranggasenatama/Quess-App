0b33ba8d65b3ca63f213278b8eb12b23a11a451f
Rangga <ranggasenatama@gmail.com>
2018-02-10 14:09:12 +0700

    http connection

--- app/src/main/java/com/example/asus/quessapp/MainActivity.java
+++ app/src/main/java/com/example/asus/quessapp/MainActivity.java
@@ -6,6 +6,7 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
 import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,10 +14,17 @@ public class MainActivity extends AppCompatActivity {
     public class DownloadTask extends AsyncTask<String, Void, Bitmap>{
 
         @Override
-        protected Bitmap doInBackground(String... strings) {
+        protected Bitmap doInBackground(String... urls) {
             String result = "";
             URL url;
             HttpURLConnection httpURLConnection;
+
+            try {
+                url = new URL(urls[0]);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
         }
     }
 
