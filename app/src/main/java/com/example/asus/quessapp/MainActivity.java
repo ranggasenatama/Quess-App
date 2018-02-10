dddadf18554a9c208f0d6c63921e56adb3dc6911
Rangga <ranggasenatama@gmail.com>
2018-02-10 18:48:26 +0700

    get url

--- app/src/main/java/com/example/asus/quessapp/MainActivity.java
+++ app/src/main/java/com/example/asus/quessapp/MainActivity.java
@@ -4,33 +4,76 @@ import android.graphics.Bitmap;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.ExecutionException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
-
-    public class DownloadTask extends AsyncTask<String, Void, Bitmap>{
-
-        @Override
-        protected Bitmap doInBackground(String... urls) {
-            String result = "";
-            URL url;
-            HttpURLConnection httpURLConnection;
-
-            try {
-                url = new URL(urls[0]);
-                httpURLConnection = (HttpURLConnection) url.openConnection();
-            } catch (Exception e) {
-                e.printStackTrace();
-            }
-        }
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        DownloadTask downloadTask = new DownloadTask();
+        String result = null;
+
+        try {
+            result = downloadTask.execute("http://www.posh24.se/kandisar").get();
+
+            String split[] = result.split("<div class=\"channelListEntry\">");
+
+            Pattern pattern = Pattern.compile("<a href=\"(.*?)\">");
+            Matcher matcher = pattern.matcher(split[0]);
+
+            while (matcher.find()) {
+                Log.i("image", matcher.group(1) + "");
+            }
+
+            pattern = Pattern.compile("<alt=\"(.*?)\">");
+            matcher = pattern.matcher(split[0]);
+
+            while (matcher.find()) {
+                Log.i("image", matcher.group(1) + "");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+
+    }
+
+    public class DownloadTask extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection httpURLConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                int data = inputStream.read();
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = inputStreamReader.read();
+                }
+                return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
     }
 }
