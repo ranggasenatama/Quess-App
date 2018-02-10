529098e4bd7516189e37863dc9235c95261d97c7
Rangga <ranggasenatama@gmail.com>
2018-02-10 18:57:52 +0700

    download image manager

--- app/src/main/java/com/example/asus/quessapp/MainActivity.java
+++ app/src/main/java/com/example/asus/quessapp/MainActivity.java
@@ -5,17 +5,19 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
-
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
 import java.net.URL;
-import java.util.concurrent.ExecutionException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class MainActivity extends AppCompatActivity {
+
+    List<String> listImage = new ArrayList<String>();
+    List<String> listNameArtis = new ArrayList<String>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,7 +25,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         DownloadTask downloadTask = new DownloadTask();
-        String result = null;
+        String result ;
 
         try {
             result = downloadTask.execute("http://www.posh24.se/kandisar").get();
@@ -34,20 +36,47 @@ public class MainActivity extends AppCompatActivity {
             Matcher matcher = pattern.matcher(split[0]);
 
             while (matcher.find()) {
-                Log.i("image", matcher.group(1) + "");
+                listImage.add(matcher.group(1));
             }
 
             pattern = Pattern.compile("<alt=\"(.*?)\">");
             matcher = pattern.matcher(split[0]);
 
             while (matcher.find()) {
-                Log.i("image", matcher.group(1) + "");
+                listNameArtis.add(matcher.group(1));
             }
         } catch (Exception e) {
             e.printStackTrace();
         }
 
 
+    }
+
+    public class DownloadImage extends AsyncTask<String, Void, Bitmap>{
+
+        @Override
+        protected Bitmap doInBackground(String... urls) {
+            String result = "";
+            URL url;
+            HttpURLConnection httpURLConnection = null;
+
+            try {
+                url = new URL(urls[0]);
+                httpURLConnection = (HttpURLConnection) url.openConnection();
+                InputStream inputStream = httpURLConnection.getInputStream();
+                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
+                int data = inputStream.read();
+                while (data != -1) {
+                    char current = (char) data;
+                    result += current;
+                    data = inputStreamReader.read();
+                }
+                return result;
+            } catch (Exception e) {
+                e.printStackTrace();
+                return null;
+            }
+        }
     }
 
     public class DownloadTask extends AsyncTask<String, Void, String> {
