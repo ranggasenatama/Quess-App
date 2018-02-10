449e80bff751df35b0981291cbb6da61fcf0f0e2
Rangga <ranggasenatama@gmail.com>
2018-02-10 19:08:10 +0700

    random

--- app/src/main/java/com/example/asus/quessapp/MainActivity.java
+++ app/src/main/java/com/example/asus/quessapp/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.asus.quessapp;
 
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,6 +12,7 @@ import java.net.HttpURLConnection;
 import java.net.URL;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -18,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
 
     List<String> listImage = new ArrayList<String>();
     List<String> listNameArtis = new ArrayList<String>();
+    int selectedCelebrity=0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,6 +48,9 @@ public class MainActivity extends AppCompatActivity {
             while (matcher.find()) {
                 listNameArtis.add(matcher.group(1));
             }
+
+            Random random = new Random();
+            selectedCelebrity = random.nextInt(listNameArtis.size());
         } catch (Exception e) {
             e.printStackTrace();
         }
@@ -56,22 +62,16 @@ public class MainActivity extends AppCompatActivity {
 
         @Override
         protected Bitmap doInBackground(String... urls) {
-            String result = "";
-            URL url;
-            HttpURLConnection httpURLConnection = null;
 
             try {
-                url = new URL(urls[0]);
-                httpURLConnection = (HttpURLConnection) url.openConnection();
-                InputStream inputStream = httpURLConnection.getInputStream();
-                InputStreamReader inputStreamReader = new InputStreamReader(inputStream);
-                int data = inputStream.read();
-                while (data != -1) {
-                    char current = (char) data;
-                    result += current;
-                    data = inputStreamReader.read();
-                }
-                return result;
+                URL url = new URL(urls[0]);
+
+                HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+                InputStream inputStream = connection.getInputStream();
+                Bitmap myBitmap = BitmapFactory.decodeStream(inputStream);
+
+                return  myBitmap;
             } catch (Exception e) {
                 e.printStackTrace();
                 return null;
